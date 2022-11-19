5ac0f2eeee51c475333df438ba5a980f3cc34c50
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2022-11-19 17:03:19 -0300

    update to adequate for java 8

--- src/main/java/br/com/helpme/helpmecore/improvement/repository/ImprovementQueries.java
+++ src/main/java/br/com/helpme/helpmecore/improvement/repository/ImprovementQueries.java
@@ -2,9 +2,5 @@ package br.com.helpme.helpmecore.improvement.repository;
 
 public interface ImprovementQueries {
 
-    public static final String FIND_ALL_BY_USER_ID = """
-                select imp from Improvement imp    
-                     join imp.user
-                  where imp.user.idUser = :userId                                           
-            """;
+    public static final String FIND_ALL_BY_USER_ID = "select imp from Improvement imp join imp.user where imp.user.idUser = :userId";
 }
